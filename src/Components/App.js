61958ee4e42a1dfbc9db92a90433ca30308b1d45
os-born <ol.serduk@gmail.com>
2021-10-25 18:56:05 +0300

    edit const

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,9 +7,9 @@ import Statistics from './Statistics';
 
 export default function App() {
   
-  let [good, setGood] = useState(0);
-  let [neutral, setNeutral] = useState(0);
-  let [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const onHandleChange = (key) => {
     switch (key) {
